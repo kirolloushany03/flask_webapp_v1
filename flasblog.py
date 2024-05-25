c41fbe004fd286a3f7494aa2fdab6dc7cf9786e9
root <root@fourthpyramid.localdomain>
2024-05-25 13:31:58 +0300

    new update

--- flasblog.py
+++ flasblog.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.column(db.integer, primary_key=True)
+    content = db.column(db.string(200), nullable=False)
+    date_created = db.column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
 @app.route("/")
 def hello():
-    return "<h1>Hellow world</h1>"
+    return render_template("index.html")
 
 
 @app.route("/about")
 def about():
-    return render_template("/templates/index.html")
+    return "hellow"
 
 
 if __name__ == "__main__":
